af8cb187537628021c1f2e57baf9f19e7ec433dc
einfachIrgendwer0815 <eavmb@gmx.de>
2021-08-20 10:46:05 +0200

    added sessions

--- backend/werwolfOnlineBackendServer.py
+++ backend/werwolfOnlineBackendServer.py
@@ -1,6 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, session
+from flask_session import Session
+
+import string
+import random
 
 app = Flask(__name__)
+app.secret_key = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(20)])
+SESSION_PERMANENT = False
 
 @app.route("/")
 def index():
