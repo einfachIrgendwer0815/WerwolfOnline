7565aaafc584b5e338b31d3dbacfa9850a87af65
einfachIrgendwer0815 <eavmb@gmx.de>
2021-08-20 11:06:44 +0200

    new favicon create new components removed content of app.component.html

--- webpage/werwolfOnline/src/app/app.module.ts
+++ webpage/werwolfOnline/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { JoinRoomComponent } from './join-room/join-room.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    CreateRoomComponent,
+    JoinRoomComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
