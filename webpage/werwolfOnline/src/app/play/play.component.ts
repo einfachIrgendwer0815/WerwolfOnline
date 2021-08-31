9f30f05c492f2c820d7a0deb26c0470b14655c6d
einfachIrgendwer0815 <eavmb@gmx.de>
2021-08-31 13:42:29 +0200

    webpage - play | Created route transition animations for sub-routes

--- webpage/werwolfOnline/src/app/play/play.component.ts
+++ webpage/werwolfOnline/src/app/play/play.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+
+import { trigger, transition } from '@angular/animations';
+
+import { slideRight, slideLeft, overlay, overlayLeft, overlayRight } from '../animations';
 
 @Component({
   selector: 'app-play',
   templateUrl: './play.component.html',
-  styleUrls: ['./play.component.scss']
+  styleUrls: ['./play.component.scss'],
+  animations: [
+    trigger('test', [
+      transition("PlayMain => PlayJoin", overlayLeft),
+      transition("PlayMain => PlayCreate", overlayLeft),
+      transition("PlayJoin => PlayMain", overlayRight),
+      transition("PlayCreate => PlayMain", overlayRight),
+    ])
+  ]
 })
 export class PlayComponent implements OnInit {
   arrowBack = faChevronLeft;
@@ -13,6 +26,11 @@ export class PlayComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  prepareRoute(outlet: RouterOutlet) {
+    console.log(outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation);
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
   }
 
 }
