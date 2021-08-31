7d86107b5ed3fc4d78189b9abddc0e29628cba1f
einfachIrgendwer0815 <eavmb@gmx.de>
2021-08-31 12:40:35 +0200

    webpage - globalStyleData, play | Added style variables, Added header for play

--- webpage/werwolfOnline/src/app/play/play.component.ts
+++ webpage/werwolfOnline/src/app/play/play.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-play',
   templateUrl: './play.component.html',
   styleUrls: ['./play.component.scss']
 })
 export class PlayComponent implements OnInit {
+  arrowBack = faChevronLeft;
 
   constructor() { }
 
