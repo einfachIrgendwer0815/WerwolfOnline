7cb4a5b6f996803c68e3f9843ed394a0600b8447
einfachIrgendwer0815 <eavmb@gmx.de>
2021-09-02 14:59:43 +0200

    backend | Removed placeholder index, Removed sessions

--- backend/werwolfOnlineBackendServer.py
+++ backend/werwolfOnlineBackendServer.py
@@ -1,16 +1,6 @@
-from flask import Flask, jsonify, session
-from flask_session import Session
-
-import string
-import random
+from flask import Flask, request
 
 app = Flask(__name__)
-app.secret_key = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(20)])
-SESSION_PERMANENT = False
-
-@app.route("/")
-def index():
-    return "Webpage"
 
 if __name__ == "__main__":
     app.run(debug=True)
