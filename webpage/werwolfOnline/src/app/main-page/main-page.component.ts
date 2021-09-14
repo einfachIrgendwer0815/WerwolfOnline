bf9f9e076e9e5de012b288fc5bece39d8db7cf2a
einfachIrgendwer0815 <eavmb@gmx.de>
2021-09-14 17:01:06 +0200

    webpage - mainPage | Added blink animation

--- webpage/werwolfOnline/src/app/main-page/main-page.component.ts
+++ webpage/werwolfOnline/src/app/main-page/main-page.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
+  letterAnimationClasses: Array<string> = [];
+  approxLength: number = 7;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.genAnimationClasses();
   }
 
+  genRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  genAnimationClasses() {
+    for(var i:number = 0; i < this.approxLength; i++) {
+      var thisClasses: string = '';
+
+      if (this.genRandomNumber(1, 5) < 4) {
+        thisClasses = 'animate';
+
+        thisClasses += ' anim' + this.genRandomNumber(1,3).toString();
+        thisClasses += ' delay' + this.genRandomNumber(1,7).toString();
+      }
+
+      this.letterAnimationClasses.push(thisClasses);
+    }
+
+  }
+
+  getAnimationClass(id: number): string {
+    if (id >= this.letterAnimationClasses.length) {
+      return '';
+    }
+
+    return this.letterAnimationClasses[id];
+  }
 }
