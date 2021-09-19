01199dc65b1d3a61bf328613f78d839d5f531a35
einfachIrgendwer0815 <eavmb@gmx.de>
2021-09-19 18:12:03 +0200

    backend | Add GameControl-startup to server

--- backend/werwolfOnlineBackendServer.py
+++ backend/werwolfOnlineBackendServer.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
+import logging
+
+from modules import gameControl
 
 app = Flask(__name__)
+app.gameControl = gameControl.GameControl()
+app.gameControl.start()
 
 if __name__ == "__main__":
     app.run(debug=True)
