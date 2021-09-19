f5701ab11f21deeda18ca9941ae79d53b5c3c113
einfachIrgendwer0815 <eavmb@gmx.de>
2021-09-19 17:14:44 +0200

    backend | Added db_system initialization

---
+++ backend/db.py
@@ -0,0 +1,13 @@
+import decoratedClasses
+import config
+
+config.read_DB_DATA()
+
+MYSQL_DB_SYSTEM = None
+
+def create_mysql_db_system():
+    global MYSQL_DB_SYSTEM
+    if MYSQL_DB_SYSTEM != None:
+        return
+
+    MYSQL_DB_SYSTEM = decoratedClasses.dbSystem(host=config.DB_DATA['host'], username=config.DB_DATA['username'], password=config.DB_DATA['password'], database=config.DB_DATA['database'])
