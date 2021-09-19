7d2ca73cd1a9815dc988c9452e713b09af32091d
einfachIrgendwer0815 <eavmb@gmx.de>
2021-09-19 16:37:18 +0200

    backend - config | Bug fix

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,10 @@ import json
 DB_DATA = None
 
 def read_DB_DATA():
+    global DB_DATA
+
+    if DB_DATA != None:
+        return
+        
     with open('dbData/data.json', 'r') as file:
         DB_DATA = json.load(file)
