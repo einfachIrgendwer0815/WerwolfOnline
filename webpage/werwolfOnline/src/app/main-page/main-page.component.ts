893399acb00ba64621c08abbe85b5e5670295135
einfachIrgendwer0815 <eavmb@gmx.de>
2021-09-28 16:53:35 +0200

    webpage - mainPage | Added letter generation to html document

--- webpage/werwolfOnline/src/app/main-page/main-page.component.ts
+++ webpage/werwolfOnline/src/app/main-page/main-page.component.ts
@@ -6,40 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
-  letterAnimationClasses: Array<string> = [];
-  approxLength: number = 7;
-
   constructor() { }
 
   ngOnInit(): void {
-    this.genAnimationClasses();
   }
 
   genRandomNumber(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  genAnimationClasses() {
-    for(var i:number = 0; i < this.approxLength; i++) {
-      var thisClasses: string = '';
-
-      if (this.genRandomNumber(1, 5) < 4) {
-        thisClasses = 'animate';
-
-        thisClasses += ' anim' + this.genRandomNumber(1,3).toString();
-        thisClasses += ' delay' + this.genRandomNumber(1,7).toString();
-      }
-
-      this.letterAnimationClasses.push(thisClasses);
-    }
-
+  parseInt(text: string | null) {
+      return parseInt(text as string);
   }
 
-  getAnimationClass(id: number): string {
-    if (id >= this.letterAnimationClasses.length) {
-      return '';
+  genAnimClass(): string {
+    var thisClasses: string = '';
+
+    if (this.genRandomNumber(1, 5) < 4) {
+      thisClasses = 'animate';
+
+      thisClasses += ' anim' + this.genRandomNumber(1,3).toString();
+      thisClasses += ' delay' + this.genRandomNumber(1,7).toString();
     }
 
-    return this.letterAnimationClasses[id];
+    return thisClasses;
   }
 }
