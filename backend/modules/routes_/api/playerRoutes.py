e678e757085c5147b4e7244c9c701cfba261d64f
einfachIrgendwer0815 <eavmb@gmx.de>
2021-10-01 19:41:31 +0200

    backend | Required jwt for player routes

--- backend/modules/routes_/api/playerRoutes.py
+++ backend/modules/routes_/api/playerRoutes.py
@@ -1,12 +1,19 @@
+from flask import request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
+
+@jwt_required()
 def isPlayerRegistered():
     return '', 200
 
+@jwt_required()
 def registerPlayer():
     return '', 200
 
+@jwt_required()
 def unregisterPlayer():
     return '', 200
 
+@jwt_required()
 def updatePlayerExpireTimestamp():
     return '', 200
 
