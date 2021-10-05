73fe10da9df3b3a3889d84211e2643d2f3920cfc
einfachIrgendwer0815 <eavmb@gmx.de>
2021-10-05 17:38:24 +0200

    webpage - howToPlay | Added back arrow

--- webpage/werwolfOnline/src/app/how-to-play/how-to-play.component.ts
+++ webpage/werwolfOnline/src/app/how-to-play/how-to-play.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-how-to-play',
   templateUrl: './how-to-play.component.html',
   styleUrls: ['./how-to-play.component.scss']
 })
 export class HowToPlayComponent implements OnInit {
+  arrowBack = faChevronRight;
 
   constructor() { }
 
