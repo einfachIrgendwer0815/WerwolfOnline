54030a10336caff528380263e3f2c42fdfa40cc7
einfachIrgendwer0815 <eavmb@gmx.de>
2021-10-26 10:50:21 +0200

    webpage - dbWatchdog | Added clearing expired players from db every minute

--- backend/dbWatchdog.py
+++ backend/dbWatchdog.py
@@ -1,6 +1,43 @@
+from modules import db
+
+import threading
+import time
+from datetime import datetime, timezone
+
 class DBWatchdog():
+    def __init__(self):
+        self.thread = WatchdogThread()
+
     def start(self):
-        pass
+        self.thread.start()
+
+    def stop(self):
+        self.thread.stop()
+
+class WatchdogThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+
+        self._doRun_ = True
+
+    def stop(self):
+        self._doRun_ = False
+
+    def run(self):
+        while self._doRun_:
+            dbSystem = db.create_mysql_db_system()
+            dbSystem.connect_to_db()
+            dbSystem.create_cursor()
+
+            timestamp = int(datetime.timestamp(datetime.now(timezone.utc)))
+            sql = f"DELETE FROM Player where expireTimestamp<={timestamp};"
+
+            dbSystem.cursor.execute(sql)
+            dbSystem.dbCon.commit()
+
+            dbSystem.close_connection()
+
+            time.sleep(60)
 
 if __name__ == "__main__":
     DBWatchdog().start()
