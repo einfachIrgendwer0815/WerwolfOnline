5857d1d95fd1b997034584d496cc7260d898f059
einfachIrgendwer0815 <eavmb@gmx.de>
2021-11-14 12:35:16 +0100

    backend - appRoutes | Adjusted urls

--- backend/modules/routes_/appRoutes.py
+++ backend/modules/routes_/appRoutes.py
@@ -1,4 +1,4 @@
-from flask import request, redirect, render_template, url_for
+from flask import request, redirect, render_template, url_for, send_from_directory
 
 import re
 
@@ -7,26 +7,33 @@ def index():
         return redirect('/de')
     return redirect('/en')
 
-def de():
-    return render_template('de/index.html')
+def de(file):
+    if file == '':
+        return render_template('de/index.html')
+    else:
+        return send_from_directory('static/de', file)
 
-def en():
-    return render_template('en/index.html')
+def en(file):
+    if file == '':
+        return render_template('en/index.html')
+    else:
+        return send_from_directory('static/en', file)
 
 def assets(filename):
-    return redirect(url_for('static', filename=f'assets/{filename}'))
+    print(filename)
+    return send_from_directory('static/assets', filename)
 
 def configureRoutes(app):
     global index
     index = app.route('/')(index)
 
     global de
-    de = app.route('/de/')(de)
-    de = app.route('/static/de/')(de)
+    de = app.route('/de/', defaults={'file': ''})(de)
+    de = app.route('/de/<path:file>')(de)
 
     global en
-    en = app.route('/en/')(en)
-    en = app.route('/static/en')(en)
+    en = app.route('/en/', defaults={'file': ''})(en)
+    en = app.route('/en/<path:file>')(en)
 
     global assets
     assets = app.route('/assets/<string:filename>')(assets)
