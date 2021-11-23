d5663054f7d2255e01ce9784a9d00a27b85b72cd
einfachIrgendwer0815 <eavmb@gmx.de>
2021-11-23 15:26:34 +0100

    backend | Added override file for secret key

--- backend/werwolfOnlineBackendServer.py
+++ backend/werwolfOnlineBackendServer.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
-import json
+import json, os
 from datetime import timedelta
 
 from modules import gameControl
@@ -22,8 +22,14 @@ app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(hours=12)
 app.register_blueprint(routes.api.blueprint)
 app.register_blueprint(routes.app.blueprint)
 
-with open('jwt_secret_key.json') as jsonFile:
-    app.config['JWT_SECRET_KEY'] = json.load(jsonFile)['key']
+if os.path.exists('jwt_secret_key.override.json'):
+    with open('jwt_secret_key.override.json', 'r') as jsonFile:
+        app.config['JWT_SECRET_KEY'] = json.load(jsonFile)['key']
+
+else:
+    print("WARING: USING FALLBACK SECRET KEY. DO NOT USE FOR PRODUCTION")
+    with open('jwt_secret_key.default.json') as jsonFile:
+        app.config['JWT_SECRET_KEY'] = json.load(jsonFile)['key']
 
 jwt = JWTManager(app)
 
