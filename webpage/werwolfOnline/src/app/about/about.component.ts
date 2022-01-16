9122a79a24e1ac27fd4fafecca2069ffd7cc27f0
einfachIrgendwer0815 <eavmb@gmx.de>
2022-01-16 18:27:56 +0100

    webpage - about | Added text in about section

--- webpage/werwolfOnline/src/app/about/about.component.ts
+++ webpage/werwolfOnline/src/app/about/about.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  icon = faChevronLeft;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  goBack() {
+    this.router.navigate(['/']);
+  }
 }
