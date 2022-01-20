2ede6e3d485fb18cb2aa5bebc79d91f8cd671e7e
einfachIrgendwer0815 <eavmb@gmx.de>
2022-01-20 17:52:02 +0100

    webpage - playCreate | Added playCreate form design

--- webpage/werwolfOnline/src/app/play/create/create.component.ts
+++ webpage/werwolfOnline/src/app/play/create/create.component.ts
@@ -6,6 +6,8 @@ import { PlayerManagementService } from '../../services/playerManagement/player-
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
 
+import { FormControl, Validators } from '@angular/forms';
+
 import { environment } from '../../../environments/environment';
 
 @Component({
@@ -14,6 +16,11 @@ import { environment } from '../../../environments/environment';
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
+  isPublic: boolean = false;
+  playerLimit = new FormControl(10, [
+    Validators.required,
+    Validators.min(4)
+  ]);
 
   constructor(private player: PlayerManagementService, private linkService: LinkService, private cookieService: CookieService, private router: Router) {
     this.linkService.setLink("/play");
@@ -35,4 +42,16 @@ export class CreateComponent implements OnInit {
     }
   }
 
+  increment() {
+    this.playerLimit.setValue(this.playerLimit.value + 1);
+  }
+
+  decrement() {
+    var old: number = this.playerLimit.value;
+    this.playerLimit.setValue(old - 1);
+
+    if (!this.playerLimit.valid) {
+      this.playerLimit.setValue(old);
+    }
+  }
 }
