ed12234b1f684c94073fb1653be411c6e232a48a
einfachIrgendwer0815 <eavmb@gmx.de>
2022-01-28 18:27:52 +0100

    webpage | Added profile-settings design

--- webpage/werwolfOnline/src/app/profile-settings/profile-settings.component.ts
+++ webpage/werwolfOnline/src/app/profile-settings/profile-settings.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faSortDown } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-profile-settings',
   templateUrl: './profile-settings.component.html',
   styleUrls: ['./profile-settings.component.scss']
 })
 export class ProfileSettingsComponent implements OnInit {
+  downArrow = faSortDown;
+  name?: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.name = "NAME";
+    }, 50);
   }
 
 }
