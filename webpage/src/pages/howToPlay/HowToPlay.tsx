eab76018cd379232b4f084c65386153557d648d6
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2022-07-15 13:31:31 +0200

    Fix list items not animating correctly

--- webpage/src/pages/howToPlay/HowToPlay.tsx
+++ webpage/src/pages/howToPlay/HowToPlay.tsx
@@ -3,9 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { IonIcon } from "@ionic/react";
 
 import './scss/HowToPlay.scss';
+import './scss/liItems.scss';
 
-function back(navigate) {
-  document.scrollingElement.scrollTo(0,0);
+function back(navigate: (route: string) => void) {
+  document.scrollingElement?.scrollTo(0,0);
 
   setTimeout(() => {
     navigate("/");
@@ -23,11 +24,11 @@ function Page_HowToPlay() {
       <h1>Wie wird gespielt?</h1>
       <main>
         <ul>
-          <li style={{"--item-delay": 0 + 's' }}>
+          <li className="animate-1">
             <span>Ziel des Spiels</span>
             <p>Das Ziel des Spiels ist es für die Werwölfe, alle Dorfbewohner zu töten. Für die Dorfbewohner gilt es, alle Werwölfe ausfindig zu machen.</p>
           </li>
-          <li style={{"--item-delay": 0.5 + 's' }}>
+          <li className="animate-2">
             <span>Ablauf</span>
             <p>Zu Beginn wird jede/r Spieler/in eine Rolle zugewiesen, die er/sie geheim halten muss. (für Auflistung der Rollen siehe unten)</p>
             <p>Eine Besonderheit hat die Rolle des Bürgermeisters oder der Bürgermeisterin. Diese Rolle wird einem/r Spieler/in zusätzlich zugeteillt.
@@ -35,7 +36,7 @@ function Page_HowToPlay() {
             <p>Nach dem jede(r) Spieler/in Kenntnis von seiner/ihrer Karte genommen hat, beginnt die erste Spielrunde.</p>
             <p>Jede Spielrunde besteht aus zwei Phasen, dem Tag und der Nacht.</p>
           </li>
-          <li style={{"--item-delay": 1 + 's' }}>
+          <li className="animate-3">
             <span>In der Nacht</span>
             <p>Alle Spieler/innen mit einer Fähigkeit können nun nacheinander ihre besonderen Spielzüge ausführen.</p>
             <p>Bsp.:<br />
@@ -48,7 +49,7 @@ function Page_HowToPlay() {
                Eventuelle rollenspezifische Ereignisse beim Tod dieser Rolle werden nun angewendet.
             </p>
           </li>
-          <li style={{"--item-delay": 1.5 + 's' }}>
+          <li className="animate-4">
             <span>Am Tag</span>
             <p>Als erstes kann nun jede/r Spieler/in (auch die Werwölfe und alle, die keine Funktionsrolle haben) eine/n andere/n auf den Verdacht hin,
       dass diese/r ein Werwolf ist, anklagen.</p>
@@ -58,7 +59,7 @@ function Page_HowToPlay() {
             </p>
             <p>Danach beginnt die Nacht erneut, bis das Spielende eintritt.</p>
           </li>
-          <li style={{"--item-delay": 2 + 's' }}>
+          <li className="animate-5">
             <span>Spielende</span>
             <p>Das Spiel endet, wenn mehr Werwölfe am Leben sind als Dorfbewohner/innen (+ Hexe). Dann haben die Werwölfe gewonnen.<br />
                Gibt es keine Werwölfe mehr, die am Leben sind, endet das Spiel ebenfalls. Nun gewinnen allerdings die Dorfbewohner bzw. alle, die keine Werwölfe sind.
